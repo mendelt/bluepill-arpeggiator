cbd05bbf56c7baf2dcb4577a5d6b20a47e7c75b3
Mendelt Siebenga <msiebenga@gmail.com>
2019-04-19 14:47:16 +0200

    Initial project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_semihosting;
+
+use cortex_m_rt::{entry};
+use cortex_m_semihosting::hprintln;
+use hal::prelude::*;
+use hal::stm32;
+
+
+#[entry]
+fn main() -> ! {
+    let _peripherals = cortex_m::Peripherals::take().unwrap();
+    let _device = stm32::Peripherals::take().unwrap();
+
+    hprintln!("Hello semihosting world").unwrap();
+
+    loop { }
+}
+
